c09a1e7cc6938bc96719fff8561927746811d1c5
sameeram <mgjaye@gmail.com>
2016-05-12 13:51:47 +0530

    Clustering configs from axis2.xml is taken from env vars

--- publisher/run.sh
+++ publisher/run.sh
@@ -1,5 +1,21 @@
-#sed 's<parameter name="localMemberHost">127.0.0.1</parameter>#<parameter name="localMemberHost">CONFIGURE THIS</parameter>#' /wso2home/wso2es-2.1.0-beta/
-sed -i "s#<url>jdbc:mysql://xxx.xxx.xx.xx:#<url>jdbc:mysql://$DB_PORT_3306_TCP_ADDR:#" /wso2home/wso2es-2.1.0-beta/repository/conf/datasources/master-datasources.xml
-echo "Db port is $DB_PORT"
-echo "DB host is $DB_PORT_3306_TCP_ADDR"
+# Update the hostname of the datasource
+sed -i "s#<url>jdbc:mysql://xxx.xxx.xx:#<url>jdbc:mysql://$DB_PORT_3306_TCP_ADDR:#" /wso2home/wso2es-2.1.0-beta/repository/conf/datasources/master-datasources.xml
+
+# Update the localMemberHost - Hostname of the current node
+echo "localMemberHost is $WSO2_LOCAL_MEMBER_HOST"
+sed -i "s#<parameter name=\"localMemberHost\">xxx.xxx.xx</parameter>#<parameter name=\"localMemberHost\">$WSO2_LOCAL_MEMBER_HOST</parameter>#" /wso2home/wso2es-2.1.0-beta/repository/conf/axis2/axis2.xml
+
+# Update the  localMemberPort - Port used by the current node to communicate with the cluster
+echo "localMemberPort is $WSO2_LOCAL_MEMBER_PORT"
+sed -i "s#<parameter name=\"localMemberPort\">xxxx</parameter>#<parameter name=\"localMemberPort\">$WSO2_LOCAL_MEMBER_PORT</parameter>#" /wso2home/wso2es-2.1.0-beta/repository/conf/axis2/axis2.xml
+
+# Update the well known member host - This should be hostname of the other node
+echo "Well known host name  is $WSO2_WK_MEMBER_HOST"
+sed -i "s#<hostName>xxx.xxx.xx</hostName>#<hostName>$WSO2_WK_MEMBER_HOST</hostName>#" /wso2home/wso2es-2.1.0-beta/repository/conf/axis2/axis2.xml
+
+# Update the carbon.xml with the host name
+echo "Carbon host name is $WSO2_CARBON_HOST"
+sed -i "s#<HostName>xxx.xxx.xx</HostName>#<HostName>$WSO2_CARBON_HOST</HostName>#" /wso2home/wso2es-2.1.0-beta/repository/conf/carbon.xml
+
+# Start the server
 sh /wso2home/wso2es-2.1.0-beta/bin/wso2server.sh
