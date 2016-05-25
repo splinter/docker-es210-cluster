f9054ea8f6d39078a62df475c36789a0c7636286
sameeram <mgjaye@gmail.com>
2016-05-25 19:25:04 +0530

    Updated the docker configs to user wso2es-2.1.0-SNAPSHOT

--- store/run.sh
+++ store/run.sh
@@ -14,30 +14,30 @@ ACTUAL_WK_MEMBER_HOST=''
 eval "ACTUAL_WK_MEMBER_HOST=\${$INTERMEDIATE_WK_HOST}"
 
 # Update the hostname of the datasource
-sed -i "s#<url>jdbc:mysql://xxx.xxx.xx:#<url>jdbc:mysql://$DB_PORT_3306_TCP_ADDR:#" /wso2home/wso2es-2.1.0/repository/conf/datasources/master-datasources.xml
+sed -i "s#<url>jdbc:mysql://xxx.xxx.xx:#<url>jdbc:mysql://$DB_PORT_3306_TCP_ADDR:#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/datasources/master-datasources.xml
 
 # Update the localMemberHost - Hostname of the current node
 echo "localMemberHost is $WSO2_LOCAL_MEMBER_HOST"
-sed -i "s#<parameter name=\"localMemberHost\">xxx.xxx.xx</parameter>#<parameter name=\"localMemberHost\">$WSO2_LOCAL_MEMBER_HOST</parameter>#" /wso2home/wso2es-2.1.0/repository/conf/axis2/axis2.xml
+sed -i "s#<parameter name=\"localMemberHost\">xxx.xxx.xx</parameter>#<parameter name=\"localMemberHost\">$WSO2_LOCAL_MEMBER_HOST</parameter>#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/axis2/axis2.xml
 
 # Update the  localMemberPort - Port used by the current node to communicate with the cluster
 echo "localMemberPort is $WSO2_LOCAL_MEMBER_PORT"
-sed -i "s#<parameter name=\"localMemberPort\">xxxx</parameter>#<parameter name=\"localMemberPort\">$WSO2_LOCAL_MEMBER_PORT</parameter>#" /wso2home/wso2es-2.1.0/repository/conf/axis2/axis2.xml
+sed -i "s#<parameter name=\"localMemberPort\">xxxx</parameter>#<parameter name=\"localMemberPort\">$WSO2_LOCAL_MEMBER_PORT</parameter>#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/axis2/axis2.xml
 
 # Update the well known member host - This should be hostname of the other node
 echo "Well known host name  is $ACTUAL_WK_MEMBER_HOST"
-sed -i "s#<hostName>xxx.xxx.xx</hostName>#<hostName>$ACTUAL_WK_MEMBER_HOST</hostName>#" /wso2home/wso2es-2.1.0/repository/conf/axis2/axis2.xml
+sed -i "s#<hostName>xxx.xxx.xx</hostName>#<hostName>$ACTUAL_WK_MEMBER_HOST</hostName>#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/axis2/axis2.xml
 
 # Update the well known member port - This should be the port of the other node
-sed -i  "s#<port>xxxx</port>#<port>$WSO2_WK_MEMBER_PORT</port>#" /wso2home/wso2es-2.1.0/repository/conf/axis2/axis2.xml
+sed -i  "s#<port>xxxx</port>#<port>$WSO2_WK_MEMBER_PORT</port>#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/axis2/axis2.xml
 
 # Update the carbon.xml with the host name
 echo "Carbon host name is $WSO2_CARBON_HOST"
-sed -i "s#<HostName>xxx.xxx.xx</HostName>#<HostName>$WSO2_CARBON_HOST</HostName>#" /wso2home/wso2es-2.1.0/repository/conf/carbon.xml
+sed -i "s#<HostName>xxx.xxx.xx</HostName>#<HostName>$WSO2_CARBON_HOST</HostName>#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/conf/carbon.xml
 
 #Update the identityProvider URL
 echo "Identity Provider URL  is $IS_PORT_9443_TCP_ADDR"
-sed -i "s#\"identityProviderURL\": \"https://xxx.xxx.xx/samlsso\"#\"identityProviderURL\": \"https://$IS_PORT_9443_TCP_ADDR:9443/samlsso\"#" /wso2home/wso2es-2.1.0/repository/deployment/server/jaggeryapps/store/config/store.json
+sed -i "s#\"identityProviderURL\": \"https://xxx.xxx.xx/samlsso\"#\"identityProviderURL\": \"https://$IS_PORT_9443_TCP_ADDR:9443/samlsso\"#" /wso2home/wso2es-2.1.0-SNAPSHOT/repository/deployment/server/jaggeryapps/store/config/store.json
 
 # Start the server
-sh /wso2home/wso2es-2.1.0/bin/wso2server.sh
+sh /wso2home/wso2es-2.1.0-SNAPSHOT/bin/wso2server.sh
