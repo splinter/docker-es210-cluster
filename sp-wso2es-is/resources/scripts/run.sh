d8fe2d64bc3da5c681a3f77085ab1d878fab626d
sameeram <mgjaye@gmail.com>
2016-05-25 16:27:45 +0530

    Changed references to 2.1.0-beta to 2.1.0

--- sp-wso2es-is/resources/scripts/run.sh
+++
@@ -1,3 +0,0 @@
-echo "ES PUB1 port is $ESPUB1_PORT"
-sed -i "s#<Offset>0</Offset>#<Offset>5</Offset>#" /wso2is-5.1.0/repository/conf/carbon.xml
-sh /wso2is-5.1.0/bin/wso2server.sh
--- temp/replace.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's#<Offset>0</Offset>#<Offset>5</Offset>#' carbon.xml
